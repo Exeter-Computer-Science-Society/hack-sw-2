b7a1247d9c1d9a0d2706eff9a87e393d94d6d7b4
wiktor wiejak <wiktor.wiejak@gmail.com>
2025-10-22 20:51:33 +0100

    Added sponsor logos, fixed the boxing issue with the sponsors, ensured all logos take up about the same amount of space, cheakily changed the footer

--- app/components/General/Sponsor/index.jsx
+++ app/components/General/Sponsor/index.jsx
@@ -1,118 +1,114 @@
-'use client'
+"use client"
 
 import SponsorCard from "./Card"
 
 const info = {
-  liminal: {
-    name: "Liminal",
-    image: "/images/sponsors/liminal.svg",
-    description:
-      "Liminal is a cybersecurity platform designed to help regulated enterprises securely adopt generative AI technologies.",
-    site: "https://www.liminal.ai",
-    linkedin: "https://www.linkedin.com/company/liminal-ai-security/",
-  },
-  dora: {
-    name: "Dora Hacks",
-    image: "/images/sponsors/dora.png",
-    description:
-      "DoraHacks is a global hackathon organization that aims to foster innovation and creativity in the tech community.",
-    site: "https://www.dorahacks.com/",
-    linkedin: "https://www.linkedin.com/company/dorahacks/",
-  },
-  exeter: {
-    name: "University of Exeter",
-    image: "/images/sponsors/exeter.gif",
-    description:
-      "The University of Exeter is a UK-based university",
-    site: "https://www.exeter.ac.uk/",
-    linkedin: "https://www.linkedin.com/school/university-of-exeter/",
-  },
-  excs: {
-    name: "Exeter Computer Science Society",
-    image: "/images/sponsors/excs.svg",
-    description:
-      "The Exeter Computer Science Society supports computer science students through hackathons, workshops, and social events",
-    site: "https://www.excs.uk/",
-    linkedin:
-      "https://www.linkedin.com/school/exeter-computing-society/",
-  },
-  mintsw: {
-    name: "Mint South West",
-    image: "/images/sponsors/mintswdark.png",
-    description:
-      "Mint South West supports the crypto community in the South West",
-    site: "https://www.mrwedge.co.uk/mintsw",
-    linkedin: "https://www.linkedin.com/company/mint-southwest/",
-  },
-  techexeter: {
-    name: "Tech Exeter",
-    image: "/images/sponsors/techexeterdark.jpg",
-    description:
-      "Tech Exeter is a community of tech enthusiasts hosting regular meetups, workshops, and conferences",
-    site: "https://www.techexeter.uk/",
-    linkedin: "https://www.linkedin.com/company/tech-exeter/",
-  },
-  hh: {
-    name: "Hacking Hub",
-    image: "/images/sponsors/HH-Logo-Full-Color.svg",
-    description:
-      "HackingHub is a cutting-edge platform for web application hacking education",
-    site: "https://www.hackinghub.io/",
-    linkedin: "https://www.linkedin.com/company/hackinghub/",
-  },
-  pydata: {
-    name: "Py Data",
-    image: "/images/sponsors/pydatabig.jpeg",
-    description:
-      "PyData Exeter hosts talks and community events",
-    site: "https://pydata.org/",
-    linkedin: "https://www.linkedin.com/company/pydata-exeter/",
-  },
-  cybersoc: {
-    name: "Exeter CyberSecurity Society",
-    image: "/images/sponsors/cybersoc.jpg",
-    description:
-      "The Exeter CyberSecurity Society is building a vibrant cyber community at the University of Exeter",
-    site: "https://my.exeterguild.com/groups/TT2W9/cyber-security-society",
-    linkedin:
-      "https://www.linkedin.com/company/exeter-cyber-security-society/",
-  },
-  secridge: {
-    name: "Sec Ridge",
-    image: "/images/sponsors/secridgeblack.png",
-    description:
-      "Sec Ridge empowers the next generation of cybersecurity professionals",
-    site: "https://www.sec-ridge.com/",
-    linkedin: "https://www.linkedin.com/company/secridge/",
-  },
-  superteam: {
-    name: "SuperTeam",
-    image: "/images/sponsors/superteam.png",
-    description:
-      "SuperTeam helps the most promising projects in the Solana ecosystem",
-    site: "https://superteam.fun/",
-    linkedin: "https://www.linkedin.com/company/superteam-talent/",
-  },
-  solana: {
-    name: "Solana",
-    image: "/images/sponsors/solana.png",
-    description:
-      "The Solana Foundation supports the decentralization, growth, and security of the Solana network",
-    site: "https://www.solana.com/",
-    linkedin: "https://www.linkedin.com/company/solana-foundation/",
-  },
-};
+    southwestcybersecuritycluster: {
+		name: "South West Cyber Security Cluster",
+		image: "/images/sponsors/swcsc.png",
+		description: "The South West Cyber Security Cluster is a community of cyber security professionals in the South West of England.",
+		site: "https://southwestcsc.org/",
+		linkedin: "https://www.linkedin.com/company/south-west-cyber-security-cluster/"
+	},
+	// liminal: {
+	// 	name: "Liminal",
+	// 	image: "/images/sponsors/liminal.svg",
+	// 	description: "Liminal is a cybersecurity platform designed to help regulated enterprises securely adopt generative AI technologies.",
+	// 	site: "https://www.liminal.ai",
+	// 	linkedin: "https://www.linkedin.com/company/liminal-ai-security/"
+	// },
+	
+	// exeter: {
+	// 	name: "University of Exeter",
+	// 	image: "/images/sponsors/exeter.gif",
+	// 	description: "The University of Exeter is a UK-based university",
+	// 	site: "https://www.exeter.ac.uk/",
+	// 	linkedin: "https://www.linkedin.com/school/university-of-exeter/"
+	// },
+    hh: {
+		name: "Hacking Hub",
+		image: "/images/sponsors/HackingHub.svg",
+		description: "HackingHub is a cutting-edge platform for web application hacking education",
+		site: "https://www.hackinghub.io/",
+		linkedin: "https://www.linkedin.com/company/hackinghub/"
+	},
+	mintsw: {
+		name: "Mint South West",
+		image: "/images/sponsors/mintsw.png",
+		description: "Mint South West supports the crypto community in the South West",
+		site: "https://www.mrwedge.co.uk/mintsw",
+		linkedin: "https://www.linkedin.com/company/mint-southwest/"
+	},
+	// techexeter: {
+	// 	name: "Tech Exeter",
+	// 	image: "/images/sponsors/techexeter.png",
+	// 	description: "Tech Exeter is a community of tech enthusiasts hosting regular meetups, workshops, and conferences",
+	// 	site: "https://www.techexeter.uk/",
+	// 	linkedin: "https://www.linkedin.com/company/tech-exeter/"
+	// },
+	
+	// pydata: {
+	// 	name: "Py Data",
+	// 	image: "/images/sponsors/pydata.png",
+	// 	description: "PyData Exeter hosts talks and community events",
+	// 	site: "https://pydata.org/",
+	// 	linkedin: "https://www.linkedin.com/company/pydata-exeter/"
+	// },
+	
+	superteam: {
+		name: "SuperTeam",
+		image: "/images/sponsors/superteam.png",
+		description: "SuperTeam helps the most promising projects in the Solana ecosystem",
+		site: "https://superteam.fun/",
+		linkedin: "https://www.linkedin.com/company/superteam-talent/"
+	},
+	solana: {
+		name: "Solana",
+		image: "/images/sponsors/solana.png",
+		description: "The Solana Foundation supports the decentralization, growth, and security of the Solana network",
+		site: "https://www.solana.com/",
+		linkedin: "https://www.linkedin.com/company/solana-foundation/"
+	},
+    checkout: {
+		name: "Checkout",
+		image: "/images/sponsors/checkout.jpeg",
+		description: "Checkout is a global payment solutions provider",
+		site: "https://www.checkout.com/",
+		linkedin: "https://www.linkedin.com/company/checkout/"
+	},
+    dora: {
+		name: "Dora Hacks",
+		image: "/images/sponsors/dora.png",
+		description: "DoraHacks is a global hackathon organization that aims to foster innovation and creativity in the tech community.",
+		site: "https://www.dorahacks.com/",
+		linkedin: "https://www.linkedin.com/company/dorahacks/"
+	},
+    excs: {
+		name: "Computer Science Society",
+		image: "/images/sponsors/excs.svg",
+		description: "The Exeter Computer Science Society supports computer science students through hackathons, workshops, and social events",
+		site: "https://excs.uk/",
+		linkedin: "https://www.linkedin.com/school/exeter-computing-society/"
+	},
+    cybersoc: {
+		name: "Cyber Security Society",
+		image: "/images/sponsors/cybersoc.png",
+		description: "The Exeter CyberSecurity Society is building a vibrant cyber community at the University of Exeter",
+		site: "https://my.exeterguild.com/groups/TT2W9/cyber-security-society",
+		linkedin: "https://www.linkedin.com/company/exeter-cyber-security-society/"
+	},
+}
 
 export default function Sponsor() {
-  const sponsors = Object.values(info);
+	const sponsors = Object.values(info)
 
-  return (
-    <section className="w-full px-8 py-16">
-      <h2 className="text-6xl font-bold text-HSWaccent mb-16 max-lg:text-lg">Meet the Sponsors</h2>
+	return (
+		<section className="w-full px-8 py-16">
+			<h2 className="text-6xl font-bold text-HSWaccent mb-16 max-lg:text-lg">Meet the Sponsors</h2>
 
-	  	<div className="mx-auto max-w-7xl">
-			<div
-				className="
+			<div className="mx-auto max-w-7xl">
+				<div
+					className="
 					grid
 					grid-cols-1
 					lg:grid-cols-4
@@ -121,17 +117,18 @@ export default function Sponsor() {
 					p-3 sm:p-4 lg:p-6
 
 				"
-			>
-				{sponsors.map((s) => (
-				<SponsorCard
-					sName={s.name}
-					sSite={s.site}
-					sSummary={s.description}
-					sImage={s.image}
-				/>
-				))}
+				>
+					{sponsors.map((s, index) => (
+						<SponsorCard
+                            key={index}
+							sName={s.name}
+							sSite={s.site}
+							sSummary={s.description}
+							sImage={s.image}
+						/>
+					))}
+				</div>
 			</div>
-		</div>
-    </section>
-  );
+		</section>
+	)
 }
