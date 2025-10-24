80509648d7292749300591c89897c9fd2619919f
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2025-10-24 12:28:31 +0100

    cleanup unused fields

--- app/components/General/Sponsor/index.jsx
+++ app/components/General/Sponsor/index.jsx
@@ -6,102 +6,57 @@ const info = {
 	liminal: {
 		name: "Liminal",
 		image: "/images/sponsors/liminal.svg",
-		description: "Liminal is a cybersecurity platform designed to help regulated enterprises securely adopt generative AI technologies.",
 		site: "https://www.liminal.ai",
-		linkedin: "https://www.linkedin.com/company/liminal-ai-security/"
 	},
 	southwestcybersecuritycluster: {
 		name: "South West Cyber Security Cluster",
 		image: "/images/sponsors/swcsc.png",
-		description: "The South West Cyber Security Cluster is a community of cyber security professionals in the South West of England.",
 		site: "https://southwestcsc.org/",
-		linkedin: "https://www.linkedin.com/company/south-west-cyber-security-cluster/"
 	},
     rootmepro: {
         name: "Root Me Pro",
         image: "/images/sponsors/rootmepro.png",
-        description: "Root Me Pro is a platform for learning and practicing hacking skills.",
         site: "https://pro.root-me.org/",
-        linkedin: "https://www.linkedin.com/company/root-me-pro/"
     },
-	// exeter: {
-	// 	name: "University of Exeter",
-	// 	image: "/images/sponsors/exeter.gif",
-	// 	description: "The University of Exeter is a UK-based university",
-	// 	site: "https://www.exeter.ac.uk/",
-	// 	linkedin: "https://www.linkedin.com/school/university-of-exeter/"
-	// },
 	hh: {
 		name: "Hacking Hub",
 		image: "/images/sponsors/HackingHub.svg",
-		description: "HackingHub is a cutting-edge platform for web application hacking education",
 		site: "https://www.hackinghub.io/",
-		linkedin: "https://www.linkedin.com/company/hackinghub/"
 	},
 	mintsw: {
 		name: "Mint South West",
 		image: "/images/sponsors/mintsw.png",
-		description: "Mint South West supports the crypto community in the South West",
 		site: "https://www.mrwedge.co.uk/mintsw",
-		linkedin: "https://www.linkedin.com/company/mint-southwest/"
 	},
-	// techexeter: {
-	// 	name: "Tech Exeter",
-	// 	image: "/images/sponsors/techexeter.png",
-	// 	description: "Tech Exeter is a community of tech enthusiasts hosting regular meetups, workshops, and conferences",
-	// 	site: "https://www.techexeter.uk/",
-	// 	linkedin: "https://www.linkedin.com/company/tech-exeter/"
-	// },
-
-	// pydata: {
-	// 	name: "Py Data",
-	// 	image: "/images/sponsors/pydata.png",
-	// 	description: "PyData Exeter hosts talks and community events",
-	// 	site: "https://pydata.org/",
-	// 	linkedin: "https://www.linkedin.com/company/pydata-exeter/"
-	// },
-
 	superteam: {
 		name: "SuperTeam",
 		image: "/images/sponsors/superteam.png",
-		description: "SuperTeam helps the most promising projects in the Solana ecosystem",
 		site: "https://superteam.fun/",
-		linkedin: "https://www.linkedin.com/company/superteam-talent/"
 	},
 	solana: {
 		name: "Solana",
 		image: "/images/sponsors/solana.png",
-		description: "The Solana Foundation supports the decentralization, growth, and security of the Solana network",
 		site: "https://www.solana.com/",
-		linkedin: "https://www.linkedin.com/company/solana-foundation/"
 	},
 	checkout: {
 		name: "Checkout.com",
 		image: "/images/sponsors/checkout.jpeg",
-		description: "Checkout is a global payment solutions provider",
 		site: "https://www.checkout.com/",
-		linkedin: "https://www.linkedin.com/company/checkout/"
 	},
 	dora: {
 		name: "Dora Hacks",
 		image: "/images/sponsors/dora.png",
-		description: "DoraHacks is a global hackathon organization that aims to foster innovation and creativity in the tech community.",
 		site: "https://www.dorahacks.com/",
-		linkedin: "https://www.linkedin.com/company/dorahacks/"
 	},
 	excs: {
 		name: "Computer Science Society",
 		image: "/images/sponsors/excs.svg",
-		description: "The Exeter Computer Science Society supports computer science students through hackathons, workshops, and social events",
 		site: "https://excs.uk/",
-		linkedin: "https://www.linkedin.com/school/exeter-computing-society/"
 	},
 	cybersoc: {
 		name: "Cyber Security Society",
 		image: "/images/sponsors/cybersoc.png",
-		description: "The Exeter CyberSecurity Society is building a vibrant cyber community at the University of Exeter",
 		site: "https://my.exeterguild.com/groups/TT2W9/cyber-security-society",
-		linkedin: "https://www.linkedin.com/company/exeter-cyber-security-society/"
 	}
 }
 
