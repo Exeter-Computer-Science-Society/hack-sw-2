17cb30afb732d0544f9d60f1a914fd66b1c81f72
wiktor wiejak <wiktor.wiejak@gmail.com>
2025-12-22 18:17:50 +0000

    Changed the top bar to contin liminal and excs logo.

--- app/components/TopBar/index.jsx
+++ app/components/TopBar/index.jsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
 export function TopBar() {
 	return (
@@ -7,30 +7,70 @@ export function TopBar() {
 			<div className="flex gap-2 justify-center">
 				<div className="flex gap-1">
 					<p className="text-HSWaccent self-center">1.</p>
-					<Link href={"/"} className="text-HSWtext self-center">HSW 2024</Link>
+					<Link
+						href={"/"}
+						className="text-HSWtext self-center"
+					>
+						HSW 2024
+					</Link>
 				</div>
 
 				<div className="flex gap-1">
 					<p className="text-HSWaccent self-center">2.</p>
-					<Link href={"/conditions/terms"} className="text-HSWtext self-center">Terms & Conditions</Link>
+					<Link
+						href={"/conditions/terms"}
+						className="text-HSWtext self-center"
+					>
+						Terms & Conditions
+					</Link>
 				</div>
 
 				<div className="flex gap-1">
 					<p className="text-HSWaccent self-center">3.</p>
-					<Link href={"/conditions/intellectual"} className="text-HSWtext self-center">Intellectual Property</Link>
+					<Link
+						href={"/conditions/intellectual"}
+						className="text-HSWtext self-center"
+					>
+						Intellectual Property
+					</Link>
 				</div>
-
 			</div>
 
 			<div className="flex gap-4 justify-center items-center">
-				<Link href="https://www.sec-ridge.com/" target="_blank" className="">
-					<Image src="/images/EX-removebg-preview.png" className="h-8 w-full object-fill" width={100} height={100} alt="not found" />
+				<Link
+					href={"https://www.becomeliminal.com/"}
+					className="flex gap-1 items-center h-8 w-fit"
+				>
+					<Image
+						src={"/images/sponsors/liminal-logo.webp"}
+						width={100}
+						height={100}
+						alt="liminal-logo"
+						className="w-8"
+					/>
+					<Image
+						src={"/images/sponsors/liminal-text.webp"}
+						width={100}
+						height={100}
+						alt="liminal-text"
+					/>
 				</Link>
+
+                <p>and</p>
+
+                <Link
+                    href={"https://excs.uk/"}
+                    className="flex gap-1 items-center h-8 w-fit"
+                >
+                    <Image
+                        src={"/images/sponsors/thumbnail_EXCS.png"}
+                        width={100}
+                        height={100}
+                        alt="excs-logo"
+                        className="w-8 h-8"
+                    />
+                </Link>
 			</div>
-
-
-
-
 		</div>
 	)
 }
