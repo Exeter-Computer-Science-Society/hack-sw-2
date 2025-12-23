820e3a406144684ef616d765d95a04e31dd82dbe
wiktor wiejak <wiktor.wiejak@gmail.com>
2025-12-23 12:38:48 +0000

    Removed the top bar 1,2,3 things.

--- app/components/TopBar/index.jsx
+++ app/components/TopBar/index.jsx
@@ -12,38 +12,7 @@ import { abcMarist } from '../../fonts';
 
 export function TopBar() {
 	return (
-		<div className="absolute top-0 left-0 right-0 w-full h-fit px-[15%] max-lg:px-[5%] py-4 flex justify-between items-center gap-4 bg-HSWsecondary z-[100] border-b border-HSWtext max-lg:text-xs">
-			<div className="flex gap-2 justify-center">
-				<div className="flex gap-1">
-					<p className="text-HSWaccent self-center">1.</p>
-					<Link
-						href={"/"}
-						className="text-HSWtext self-center"
-					>
-						HSW 2024
-					</Link>
-				</div>
-
-				<div className="flex gap-1">
-					<p className="text-HSWaccent self-center">2.</p>
-					<Link
-						href={"/conditions/terms"}
-						className="text-HSWtext self-center"
-					>
-						Terms & Conditions
-					</Link>
-				</div>
-
-				<div className="flex gap-1">
-					<p className="text-HSWaccent self-center">3.</p>
-					<Link
-						href={"/conditions/intellectual"}
-						className="text-HSWtext self-center"
-					>
-						Intellectual Property
-					</Link>
-				</div>
-			</div>
+		<div className="absolute top-0 left-0 right-0 w-full h-fit px-[15%] max-lg:px-[5%] py-4 flex justify-end items-center gap-4 bg-HSWsecondary z-[100] border-b border-HSWtext max-lg:text-xs">
 
 			<div className="flex gap-4 justify-center items-center">
 				<Link
